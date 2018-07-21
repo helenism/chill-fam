4381e38f3d4520fb9c9362759b5a9331a2f24c94
Helen O <h.onunwa@gmail.com>
2018-07-21 14:47:13 +0100

    Update
    - Refresh CSS with CMD/Ctrl + shift + R

--- chillfam.py
+++ chillfam.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask('chillfam')
 
 @app.route('/')
-def index():
+def home():
     return render_template('home.html')
+
+@app.route('/food')
+def food():
+    return render_template('food.html')
+
+@app.route('/activities')
+def activities():
+    return render_template('activities.html')
+
+@app.route('/music')
+def music():
+    return render_template('music.html')
 
 @app.route('/yourfam')
 def yourfam():
